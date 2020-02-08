102aa54f31072d741a3b52d3ccb17c9f0109c55c
LasseurCoralie <coralielasseur@gmail.com>
2020-02-08 22:48:27 +0100

    appli fonctionnelle

--- js/app.js
+++ js/app.js
@@ -3,32 +3,41 @@ const app = {
   init: function() {
     // recup id bouton
     const button = document.getElementById('button-timer');
-    //  recup id zone résultat + initialisation
-    const resultTxt = document.getElementById('result');
-    resultTxt.innerText = 0;
+    button.innerText = 10;
 
+    // Je cache la modale dès le début
+    // document.getElementById('modal').style.visibility = "hidden";
+
+
+    // ecoute du click et modification de sa valeur
+      let counter = 10;
+
+      let timer = setInterval(function(){
+        button.innerText = counter;
+        // Compteur descend d'une seconde
+        counter--;
+              
+        if(counter === 0){
+          setTimeout(function(){
+            // changement texte dans la zone du bouton
+            button.innerText = 'calm down dude!'
+            clearInterval(timer);
+            button.setAttribute('disabled', true);
+            document.getElementById('modal').style.display = 'block';
+          }, 1000);
+        }
+      }, 1000);
+
+      button.addEventListener('click', function(){
+      document.querySelector('#result').innerText++;
+      });
+
+    
 
     // Partie compteur
-    let counter = 2;
+    
 
-    let timer = setInterval(function(){
-      button.innerText = counter;
-      console.log(counter);
-      counter-- ;
-      if(counter === 0){
 
-        button.addEventListener('click', function(){
-          let result = resultTxt.innerText++;
-        });
-
-        setTimeout(function(){
-          // changement texte dans la zone du bouton
-          button.innerText = 'calm down dude!'
-          clearInterval(timer);
-          button.setAttribute('disabled', true);
-        }, 1000);
-      }
-    }, 1000);
   },
 };
 
